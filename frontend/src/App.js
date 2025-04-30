b6f1e03e12d9522d721497eadb0d0e013589e89b
andiya <aufaandiya@gmail.com>
2025-04-30 15:03:44 +0700

    landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          project
-        </a>
-      </header>
+    <div className="landing-page">
+      <div className="left-section">
+        <img src="./images/bmw-logo.png" alt="BMW Logo" className="bmw-logo" />
+        <h1 className="performance-text">THE <span className="highlight">PERFORMANCE</span> <br></br>TO GO <span className="highlight">FAST.</span></h1>
+        <h1 className="technology-text">THE TECHNOLOGY <br></br><span className="highlight">TO GO FAR.</span></h1>
+      </div>
+      <div className="right-section">
+        <blockquote className="quote">
+          "BMW technology pushes the limits of the future, blending unmatched performance, innovation, and sustainability for a smarter, more efficient driving experience."
+        </blockquote>
+        <img src="./images/bmw-i8.png" alt="BMW i8" className="bmw-i8" />
+        <h2>BMW i8</h2>
+      </div>
     </div>
   );
 }
